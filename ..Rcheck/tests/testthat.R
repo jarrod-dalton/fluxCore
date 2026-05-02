9814f142590e44281e2c0059fb219fead8292ba0
jarrod-dalton <jarrod-dalton@users.noreply.github.com>
2026-05-02 12:57:46 -0400

    chore: remove accidental ..Rcheck/ artifact; update .gitignore

--- ..Rcheck/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(fluxCore)
-
-test_check("fluxCore")
--- ..Rcheck/tests/testthat/test_schema_required.R
+++
@@ -1,6 +0,0 @@
-test_that("Entity$new requires explicit schema", {
-  expect_error(
-    Entity$new(init = list(x = 1)),
-    "schema is required"
-  )
-})
